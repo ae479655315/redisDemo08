a5e3cf68a2806cd6030b52539e10735507d3dcaa
ae479655315 <39586223+ae479655315@users.noreply.github.com>
2019-07-10 10:25:46 +0800

    111234

--- src/test/java/com/aishang/Demo.java
+++ src/test/java/com/aishang/Demo.java
@@ -16,12 +16,12 @@ public class Demo {
         //开启连接
         Jedis jedis = new Jedis("localhost", 6379);
         //切换库（默认0库）
-        jedis.select(0);
+        jedis.select(1);
         //存放数据
         jedis.set("hello", "你好啊");
         //获取数据
         String string = jedis.get("hello");
-        //输出打印
+        //输出打印12321
         System.out.println(string);
         //回收资源
         jedis.close();
